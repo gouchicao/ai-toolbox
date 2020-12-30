08d185e2e8a5c19d33e74cca42c20391e172041b
wang-junjian <wang-junjian@qq.com>
2020-12-31 01:02:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-python3 create_yolov5_from_voc.py -v '/Users/wjj/work/带电检测/infrared_temperature_measurement/voc' \
-        -p '/Users/wjj/work/带电检测/infrared_temperature_measurement/project'
+python3 create_yolov5_from_voc.py -v 'voc' -p 'project'
